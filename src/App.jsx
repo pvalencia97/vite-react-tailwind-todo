387af9c9fd22bbca61a4e4d1b64544b258785eee
Jose Valencia <76021700+pvalencia97@users.noreply.github.com>
2025-05-15 13:21:27 -0600

    dia 1

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import CrossIcon from "./componets/Icons/CrossIcon";
+import MoonIcon from "./componets/Icons/MoonIcon";
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
+      <header className="container mx-auto px-4 pt-8">
+        <div className="flex justify-between">
+          <h1 className="text-3x1 font-semibold uppercase tracking-[0.3em] text-white">Todo</h1>
+          <button>
+            <MoonIcon/>
+          </button>
+        </div>
+        <form className="mt-8 flex items-center gap-4 overflow-hidden rounded-md bg-white py-4 px-4">
+          <span className="inline-block h-5 w-5 rounded-full border-2"></span>
+          <input 
+            type="text" 
+            placeholder="Create a new todo..."
+            className="w-full text-gray-400 outline-none"
+            />
+        </form>
+      </header>
+
+      <main className="container mx-auto mt-8 px-4">
+        <div className="rounded-md bg-white [&>article]:p-4">
+          <article className="flex gap-4 border-b border-b-gray-400 ">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+            <p className="grow text-gray-600">Complete only js curse in bluuweb</p>
+            <button className="flex-none">
+                <CrossIcon/>
+            </button>
+          </article>
+          <article className="flex gap-4 border-b border-b-gray-400 ">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+            <p className="grow text-gray-600">Complete only js curse in bluuweb</p>
+            <button className="flex-none">
+                <CrossIcon/>
+            </button>
+          </article>
+          <article className="flex gap-4 border-b border-b-gray-400">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+            <p className="grow text-gray-600">Complete only js curse in bluuweb</p>
+            <button className="flex-none">
+                <CrossIcon/>
+            </button>
+          </article>
+
+          <section className="flex justify-between p-4"> 
+            <span className="text-gray-400">5 items left</span>
+            <button className="text-gray-400">Clear completed</button>
+            </section>
+        </div>
+      </main>
+
+      <section className="container mx-auto mt-8 px-4">
+        <div className="flex justify-center gap-4 rounded-md bg-white p-4">
+          <button className="text-blue-600">All</button>
+          <button className="hover:text-blue-600">Active</button>
+          <button className="hover:text-blue-600">Completed</button>
+        </div>
+      </section>
+
+      <p className="text-center">Drag and drop</p>
+      
+    </div>
+  );
+};
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- esto es clave
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
